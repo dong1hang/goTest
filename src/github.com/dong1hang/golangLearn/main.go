06e952a8c8952eb64c75176e0a5a7e973cbfaa7c
Sven <dong3hang@gmail.com>
2020-11-10 23:24:59 +0900

    update

--- src/github.com/dong1hang/golangLearn/main.go
+++ src/github.com/dong1hang/golangLearn/main.go
@@ -1,7 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+
+	"golang.org/x/net/html/charset"
+
+	"golang.org/x/text/encoding"
+	"golang.org/x/text/transform"
+)
 
 func main() {
-	fmt.Println("22")
+	resp, err := http.Get("https://www.zhenai.com/zhenghun")
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		fmt.Println("Error : status code", resp.StatusCode)
+		return
+	}
+	e := determineEncoding(resp.Body)
+	utf8Reader := transform.NewReader(
+		resp.Body, e.NewDecoder())
+	fmt.Println(utf8Reader)
+	all, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%s\n", all)
+
+}
+
+func determineEncoding(r io.Reader) encoding.Encoding {
+	bytes, err := bufio.NewReader(r).Peek(1024)
+
+	bufio.NewReader(r)
+	if err != nil {
+		panic(err)
+	}
+	e, _, _ := charset.DetermineEncoding(bytes, "")
+	return e
 }
